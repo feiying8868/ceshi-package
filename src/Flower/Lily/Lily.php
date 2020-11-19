38d812302b4f9084648696679330ffbb022b27bd
qiaohuan <shandongqiaohuan@163.com>
2020-11-19 13:48:22 +0800

    代码提交

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiaohuan
+ * Date: 2020/11/19
+ * Time: 13:46
+ */
+namespace Flower\Lily;
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose/Rose.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: qiaohuan
+ * Date: 2020/11/19
+ * Time: 13:46
+ */
+namespace Flower\Rose;
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
